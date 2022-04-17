6c83e300e073cd067c83ac8dd28c07f59627d29a
Haseeb <haseeb2k11@gmail.com>
2022-04-17 03:39:08 -0400

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (isNaN(num1) || isNaN(num2)) {
+        return "ERROR";
+    }
+    const highNum = Math.max(num1, num2);
+    let finalNum = 0
+    for (i = 0; i <= highNum; i++) {
+        finalNum += i;
+    }
+    return finalNum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
