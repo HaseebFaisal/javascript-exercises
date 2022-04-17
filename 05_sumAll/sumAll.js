51941d512ab06c98415846e8d66334cbaf1109a1
Haseeb <haseeb2k11@gmail.com>
2022-04-17 04:05:28 -0400

    added paramater checker

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,15 +2,16 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
-    else if (isNaN(num1) || isNaN(num2)) {
-        return "ERROR";
-    }
-    const highNum = Math.max(num1, num2);
+    else if (typeof(num1) === 'number' && typeof(num2) === 'number') {
+        const highNum = Math.max(num1, num2);
     let finalNum = 0
     for (i = 0; i <= highNum; i++) {
         finalNum += i;
     }
-    return finalNum;
+    return finalNum
+    } else {
+        return 'ERROR';
+    }
 };
 // Do not edit below this line
 module.exports = sumAll;
